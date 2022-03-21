885984627893cca6c6956e627223825d5d567807
jaehokimdev <Jaeho.kim.Dev@gmail.com>
2022-03-20 20:32:25 -0600

    complete

--- src/sait/frms/gui/FlightsTab.java
+++ src/sait/frms/gui/FlightsTab.java
@@ -5,11 +5,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.*;
 import javax.swing.event.*;
 
-import sait.frms.gui.FlightsTab.MyListSelectionListener;
 import sait.frms.manager.FlightManager;
 import sait.frms.manager.ReservationManager;
 import sait.frms.problemdomain.Flight;
@@ -18,40 +18,38 @@ import sait.frms.problemdomain.Flight;
  * Holds the components for the flights tab.
  * 
  */
-public class FlightsTab extends TabBase implements ActionListener
-{
+public class FlightsTab extends TabBase implements ActionListener {
 	/**
 	 * Instance of flight manager.
 	 */
 //	private FlightManager flightManager;
 	private FlightManager flightManager;
-		
+
 	/**
 	 * Instance of reservation manager.
 	 */
 	private ReservationManager reservationManager;
-	
+
 	/**
 	 * List of flights.
 	 */
-	private JList<Flight> flightsList;
+	private JList<String> flightsList;
+
+	private DefaultListModel<String> flightsModel;
 	
-	private DefaultListModel<Flight> flightsModel;
-	
+	private ArrayList<Flight> findflight;
+
 	/**
 	 * Creates the components for flights tab.
 	 */
 	/**
 	 * Creates the components for flights tab.
 	 * 
-	 * @param flightManager Instance of FlightManager.
+	 * @param flightManager      Instance of FlightManager.
 	 * @param reservationManager Instance of ReservationManager
-	 * @throws FileNotFoundException 
+	 * @throws FileNotFoundException
 	 */
-	
-	private String fromValue;
-	private String toValue;
-	private String dayValue;
+
 	private JComboBox fromlist;
 	private JComboBox tolist;
 	private JComboBox daylist;
@@ -62,32 +60,31 @@ public class FlightsTab extends TabBase implements ActionListener
 	JTextField costText;
 	JTextField nameText;
 	JTextField citizenshipText;
-	
-	
+
 	public FlightsTab(FlightManager flightManager, ReservationManager reservationManager) throws FileNotFoundException {
 		this.flightManager = flightManager;
 		this.reservationManager = reservationManager;
-		
+
 		panel.setLayout(new BorderLayout());
-		
+
 		JPanel northPanel = createNorthPanel();
 		panel.add(northPanel, BorderLayout.NORTH);
-		
+
 		JPanel centerPanel = createCenterPanel();
 		panel.add(centerPanel, BorderLayout.CENTER);
-		
+
 		JPanel southPanel = createSouthPanel();
 		panel.add(southPanel, BorderLayout.SOUTH);
 	}
-	
+
 	private JPanel createSouthPanel() throws FileNotFoundException {
 		JPanel panel = new JPanel();
-		
+
 		panel.setLayout(new BorderLayout());
 		panel.add(getSouthNorthPanel(), BorderLayout.NORTH);
 		panel.add(getSouthCenterPanel(), BorderLayout.CENTER);
 		panel.add(getSouthSouthPanel(), BorderLayout.SOUTH);
-		
+
 		return panel;
 	}
 
@@ -98,41 +95,39 @@ public class FlightsTab extends TabBase implements ActionListener
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				fromValue = fromlist.getSelectedItem().toString();
-				toValue = tolist.getSelectedItem().toString();
-				dayValue = daylist.getSelectedItem().toString();
-				flightManager.findFlights(fromValue, toValue, dayValue);
+				String fromValue = fromlist.getSelectedItem().toString();
+				String toValue = tolist.getSelectedItem().toString();
+				String dayValue = daylist.getSelectedItem().toString();
+				findflight = flightManager.findFlights(fromValue, toValue, dayValue);
 				printflight();
-				getCenterCenterPanel();
 			}
-			
+
 		});
 		panel.add(findflightButton);
 		panel.setVisible(true);
 		return panel;
 	}
 
-	private JPanel getSouthCenterPanel() throws FileNotFoundException {		
-		flightManager = new FlightManager();
-		String weekdaylist[] = {"Any", "Sunday", "Monday", "Tuesday", "Wednessday", "Thursday", "Friday", "Saturday"};
+	private JPanel getSouthCenterPanel() throws FileNotFoundException {
+		String weekdaylist[] = { FlightManager.WEEKDAY_ANY, FlightManager.WEEKDAY_SUNDAY, FlightManager.WEEKDAY_MONDAY,
+				FlightManager.WEEKDAY_TUESDAY, FlightManager.WEEKDAY_WEDNESDAY, FlightManager.WEEKDAY_THURSDAY,
+				FlightManager.WEEKDAY_FRIDAY, FlightManager.WEEKDAY_SATURDAY };
 
-		
 		JPanel panel = new JPanel();
-		GridLayout gridLayout = new GridLayout(3,2);
+		GridLayout gridLayout = new GridLayout(3, 2);
 		panel.setLayout(gridLayout);
 		JLabel from = new JLabel("From:");
 		from.setFont(new Font("serif", Font.PLAIN, 13));
 		fromlist = new JComboBox(flightManager.getAirports().toArray());
-		
+
 		JLabel to = new JLabel("To:");
 		to.setFont(new Font("serif", Font.PLAIN, 13));
 		tolist = new JComboBox(flightManager.getAirports().toArray());
-		
+
 		JLabel day = new JLabel("Day:");
 		day.setFont(new Font("serif", Font.PLAIN, 13));
 		daylist = new JComboBox(weekdaylist);
-		
-		
+
 		panel.setVisible(true);
 		panel.add(from);
 		panel.add(fromlist);
@@ -140,7 +135,7 @@ public class FlightsTab extends TabBase implements ActionListener
 		panel.add(tolist);
 		panel.add(day);
 		panel.add(daylist);
-		
+
 		return panel;
 	}
 
@@ -154,21 +149,22 @@ public class FlightsTab extends TabBase implements ActionListener
 
 	/**
 	 * Creates the north panel.
+	 * 
 	 * @return JPanel that goes in north.
 	 */
-	private JPanel createNorthPanel() 
-	{
+	private JPanel createNorthPanel() {
 		JPanel panel = new JPanel();
-		
+
 		JLabel title = new JLabel("Flights", SwingConstants.CENTER);
 		title.setFont(new Font("serif", Font.PLAIN, 29));
 		panel.add(title);
-		
+
 		return panel;
 	}
-	
+
 	/**
 	 * Creates the center panel.
+	 * 
 	 * @return JPanel that goes in center.
 	 */
 	private JPanel createCenterPanel() 
@@ -176,22 +172,45 @@ public class FlightsTab extends TabBase implements ActionListener
 		JPanel panel = new JPanel();
 		
 		panel.setLayout(new BorderLayout());
-		panel.add(getCenterCenterPanel(), BorderLayout.CENTER);
+
+		flightsModel = new DefaultListModel<>();
+		flightsList = new JList<>(flightsModel);
+
+		// User can only select one item at a time.
+		flightsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+		// Wrap JList in JScrollPane so it is scrollable.
+		JScrollPane scrollPane = new JScrollPane(this.flightsList);
+
+		flightsList.addListSelectionListener(new ListSelectionListener(){
+	
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				String selectedFlightCode = flightsList.getSelectedValue().substring(0, 7);
+				Flight selectedFlight = flightManager.findFlightByCode(selectedFlightCode);
+				flightText.setText(selectedFlight.getCode());
+				airlineText.setText(selectedFlight.getAirlineName());
+				dayText.setText(selectedFlight.getWeekday());
+				timeText.setText(selectedFlight.getTime());
+				costText.setText(String.valueOf("$" + selectedFlight.getCostPerSeat()));
+			}
+				
+		});
+		
+		scrollPane.setPreferredSize(new Dimension(450, 280));
+		panel.add(scrollPane);
 		panel.add(getCenterEastPanel(), BorderLayout.EAST);
 
-		
 		return panel;
 	}
-	
+
 	private JPanel getCenterEastPanel() {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
-		
 
 		panel.add(getCenterEastNorthPanel(), BorderLayout.NORTH);
 		panel.add(getCenterEastCenterPanel(), BorderLayout.CENTER);
 		panel.add(getCenterEastSouthPanel(), BorderLayout.SOUTH);
-	
 
 		return panel;
 	}
@@ -199,6 +218,15 @@ public class FlightsTab extends TabBase implements ActionListener
 	private JPanel getCenterEastSouthPanel() {
 		JPanel panel = new JPanel();
 		JButton reserveButton = new JButton("Reserve");
+		reserveButton.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e) 
+			{
+				
+			}	
+				
+		});
 		panel.add(reserveButton);
 		return panel;
 
@@ -206,9 +234,9 @@ public class FlightsTab extends TabBase implements ActionListener
 
 	private JPanel getCenterEastCenterPanel() {
 		JPanel panel = new JPanel();
-		GridLayout gridLayout = new GridLayout(7,2);
+		GridLayout gridLayout = new GridLayout(7, 2);
 		panel.setLayout(gridLayout);
-		
+
 		JLabel flight = new JLabel("Flight:");
 		flight.setFont(new Font("serif", Font.PLAIN, 13));
 		flightText = new JTextField(20);
@@ -240,10 +268,8 @@ public class FlightsTab extends TabBase implements ActionListener
 
 		JLabel citizenship = new JLabel("Citizenship:");
 		citizenship.setFont(new Font("serif", Font.PLAIN, 13));
-		citizenshipText = new JTextField(20);		
-		
+		citizenshipText = new JTextField(20);
 
-		
 		panel.add(flight);
 		panel.add(flightText);
 		panel.add(airline);
@@ -258,10 +284,9 @@ public class FlightsTab extends TabBase implements ActionListener
 		panel.add(nameText);
 		panel.add(citizenship);
 		panel.add(citizenshipText);
-		
-		panel.setSize(300,300);
-		panel.setVisible(true);
 
+		panel.setSize(300, 300);
+		panel.setVisible(true);
 
 		return panel;
 	}
@@ -274,49 +299,13 @@ public class FlightsTab extends TabBase implements ActionListener
 		return panel;
 	}
 
-	private JPanel getCenterCenterPanel() 
-	{ 
-    JPanel panel = new JPanel();
-    flightsModel = new DefaultListModel<>();
-    
-
-	flightsList = new JList<>(flightsModel);
-	
-	// User can only select one item at a time.
-	flightsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-	
-	// Wrap JList in JScrollPane so it is scrollable.
-	JScrollPane scrollPane = new JScrollPane(this.flightsList);
-	
-	flightsList.addListSelectionListener(new MyListSelectionListener());
-	scrollPane.setPreferredSize(new Dimension(450,280));
-	panel.add(scrollPane);
-	
-	return panel; 
-	}
-	
-	public void printflight()
-	{
-		flightsModel.clear();
-		for (int i = 0; i < flightManager.getfindFlight().size() ; i++)
-	    {
-	    	flightsModel.addElement(flightManager.getfindFlight().get(i));
-	    }
-	}
-
-
-	public class MyListSelectionListener implements ListSelectionListener 
-	{
-		/**
-		 * Called when user selects an item in the JList.
-		 */
-		@Override
-		public void valueChanged(ListSelectionEvent e) {
-			
-		}
+	public void printflight() {
 		
+		flightsModel.clear();
+		for (int i = 0; i < findflight.size(); i++) {
+			flightsModel.addElement(findflight.get(i).toString());
+		}
 	}
-
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -324,7 +313,4 @@ public class FlightsTab extends TabBase implements ActionListener
 		
 	}
 
-
-
-	
 }
